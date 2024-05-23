0b81193380dab1e2cf6a5c40048bee2be13c987f
Maciej_Malecki_2Tc <154277219+MaciejTc@users.noreply.github.com>
2024-05-23 12:54:47 +0200

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,17 @@
+public class Node {
+    private static long counter = 0;
+    public final long id;
+
+    public int previousID;
+    public int distance;
+
+    public Node() {
+        synchronized (Node.class) {
+            this.id = ++counter;
+        }
+    }
+
+    public int getDistance() {
+        return this.distance;
+    }
+}
